305ef4f2593eb7f5f5115b165aff76bcc6b33d49
Thomas Casady <tscasady@gmail.com>
2018-02-15 04:47:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
-function destructivelyAppendKitten(array){
-  array.push()
+function destructivelyAppendKitten(array, element){
+  array.push(element)
   return array
 }
